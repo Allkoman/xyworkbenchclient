bd47ff94558f130838a42968170a690aafada82c
panglong@T440p <hitpanglong@gamil.com>
2016-07-15 17:33:00 +0800

    add home page

--- app/app.js
+++ app/app.js
@@ -6,12 +6,17 @@ angular.module('XYWorkbench', [
   'ngAnimate',
   'ui.bootstrap',
   'XYWorkbench.Index',
+  'XYWorkbench.Home',
   'myApp.view1',
   'myApp.view2',
   'myApp.version'
 ]).
 config(['$locationProvider', '$routeProvider', function($locationProvider, $routeProvider) {
-  $locationProvider.hashPrefix('!');
-
-  $routeProvider.otherwise({redirectTo: '/view1'});
+        //$locationProvider.hashPrefix('!');
+        //Otherwise, the href location has to be prefixed with !
+        $routeProvider.
+                when('/Home',{
+                    templateUrl:'Home/tmpl/Home.html'
+                }).
+                otherwise({redirectTo: '/view1'});
 }]);
